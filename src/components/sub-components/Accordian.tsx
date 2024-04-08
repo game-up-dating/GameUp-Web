fb8da84b048a5d3b516278248f1e6b63c77ef7e4
root-daemon <srivishal.sivasubramanian@gmail.com>
2024-04-08 23:27:58 +0530

    Accordion

---
+++ src/components/sub-components/Accordian.tsx
@@ -0,0 +1,48 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+
+export function AccordianComponent() {
+  return (
+    <div className="flex justify-center items-center">
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>What is GameUp</AccordionTrigger>
+          <AccordionContent>
+            Gameup is an Ai-assisted chatting app designed to enhance user’s
+            “dating skills. It uses Ai-Generated suggestions based on user
+            upload conversations screenshots and prefered “Flirtiness Level”
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>What is GameUp</AccordionTrigger>
+          <AccordionContent>
+            Gameup is an Ai-assisted chatting app designed to enhance user’s
+            “dating skills. It uses Ai-Generated suggestions based on user
+            upload conversations screenshots and prefered “Flirtiness Level”
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item-3">
+          <AccordionTrigger>What is GameUp</AccordionTrigger>
+          <AccordionContent>
+            Gameup is an Ai-assisted chatting app designed to enhance user’s
+            “dating skills. It uses Ai-Generated suggestions based on user
+            upload conversations screenshots and prefered “Flirtiness Level”
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-4">
+          <AccordionTrigger>What is GameUp</AccordionTrigger>
+          <AccordionContent>
+            Gameup is an Ai-assisted chatting app designed to enhance user’s
+            “dating skills. It uses Ai-Generated suggestions based on user
+            upload conversations screenshots and prefered “Flirtiness Level”
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+}
